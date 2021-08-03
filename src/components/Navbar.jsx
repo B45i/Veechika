31382e8f0497c0dcaf6d8cd5611597839b097179
B45i <amalshajan2011@gmail.com>
2021-08-04 02:49:45 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h2>Veechika</h2>
+            <p className="app-subtitle">Online Malayalam Radio</p>
+        </nav>
+    );
+};
+
+export default Navbar;
