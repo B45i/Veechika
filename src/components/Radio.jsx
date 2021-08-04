5cd93b5b9be92a2c9fa4e2d8dad6ea567fbe0e88
B45i <amalshajan2011@gmail.com>
2021-08-04 09:03:04 +0530

    station selection

--- src/components/Radio.jsx
+++ src/components/Radio.jsx
@@ -1,20 +1,35 @@
+import { useState } from 'react';
 import { stationList } from '../data/stations';
 
 import './Radio.css';
 
 const Radio = () => {
+    const [nowPlaying, setNowPlaying] = useState();
+
+    const play = station => {
+        setNowPlaying(station);
+    };
+
     return (
         <div className="radio">
             <ul className="stations">
                 {(stationList || []).map(station => (
-                    <li className="station" key={station.url}>
+                    <li
+                        className={`station ${
+                            nowPlaying === station ? 'station-selcted' : ''
+                        }`}
+                        key={station.url}
+                    >
                         <img
                             src={station.img}
                             alt={station.img}
                             className="station-img"
                         />
                         <div className="station-name">{station.name}</div>
-                        <button className="btn-play">
+                        <button
+                            className="btn-play"
+                            onClick={e => play(station)}
+                        >
                             <i className="icon-play"></i>
                         </button>
                     </li>
