05603c7ac050204477e6c8552b70d26f375f65b3
b45i <amalshajan2011@gmail.com>
2022-02-24 23:13:18 +0530

    remove video.js

--- src/components/RadioPlayer.jsx
+++ src/components/RadioPlayer.jsx
@@ -1,35 +1,6 @@
 import { useEffect, useRef } from 'react';
-import videojs from 'video.js';
 
 import './RadioPlayer.css';
-
-export const VideoJS = props => {
-    const videoRef = useRef(null);
-    const { options } = props;
-
-    const VideoHtml = props => (
-        <div className="hidden-player" data-vjs-player>
-            <video ref={videoRef} className="video-js vjs-big-play-centered" />
-        </div>
-    );
-
-    useEffect(() => {
-        const videoElement = videoRef.current;
-        let player;
-        if (videoElement) {
-            player = videojs(videoElement, options, () => {
-                console.log('player is ready');
-            });
-        }
-        return () => {
-            if (player) {
-                player.dispose();
-            }
-        };
-    }, [options]);
-
-    return <VideoHtml />;
-};
 
 const RadioPlayer = ({ name, img, url, isPlaying, togglePlay }) => {
     return name ? (
@@ -39,21 +10,6 @@ const RadioPlayer = ({ name, img, url, isPlaying, togglePlay }) => {
             <button className="btn-player" onClick={togglePlay}>
                 <i className={isPlaying ? 'icon-pause' : 'icon-play'}></i>
             </button>
-
-            <VideoJS
-                options={{
-                    autoplay: true,
-                    sources: [
-                        {
-                            src: url,
-                            type:
-                                url.split('.').splice(-1) === 'm3u8'
-                                    ? 'application/x-mpegURL'
-                                    : 'audio/mp3',
-                        },
-                    ],
-                }}
-            />
         </div>
     ) : null;
 };
