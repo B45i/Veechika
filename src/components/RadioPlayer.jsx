b80090d8a961b070ff1210631df6401768b44e85
b45i <amalshajan2011@gmail.com>
2022-02-25 17:40:18 +0530

    hls player

--- src/components/RadioPlayer.jsx
+++ src/components/RadioPlayer.jsx
@@ -1,8 +1,41 @@
 import { useEffect, useRef } from 'react';
+import Hls from 'hls.js';
 
 import './RadioPlayer.css';
 
+const videoElement = document.getElementById('videoTag');
 const RadioPlayer = ({ name, img, url, isPlaying, togglePlay }) => {
+    const src =
+        'https://air.pc.cdn.bitgravity.com/air/live/pbaudio044/playlist.m3u8';
+    const firstRender = useRef(true);
+    useEffect(() => {
+        if (!url) {
+            return;
+        }
+        if (Hls.isSupported() || src.includes('.m3u8')) {
+            const hls = new Hls();
+            hls.loadSource(src);
+            hls.attachMedia(videoElement);
+            hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                if (!isPlaying) {
+                    videoElement.play();
+                }
+            });
+        }
+    }, [url]);
+
+    useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false;
+            return;
+        }
+        if (isPlaying) {
+            videoElement.pause();
+        } else {
+            videoElement.play();
+        }
+    }, [isPlaying]);
+
     return name ? (
         <div className="radio-player">
             <img className="song-img" src={img} alt={img} />
