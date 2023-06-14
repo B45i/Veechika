63358a8eade3c0b10a29d58909aa1d805064f56d
B45i <amalshajan2011@gmail.com>
2023-06-15 01:17:32 +0530

    feedback option

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,17 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h2>വീചിക</h2>
-            <p className="app-subtitle">Online Malayalam Radio</p>
+            <div>
+                <h2>വീചിക</h2>
+                <p className="app-subtitle">Online Malayalam Radio</p>
+            </div>
+            <a
+                href="https://bit.ly/veechika"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Feedback
+            </a>
         </nav>
     );
 };
