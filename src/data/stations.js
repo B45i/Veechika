72d4858af8becf4958bd2de2db83e7585cc54734
B45i <amalshajan2011@gmail.com>
2023-06-15 00:29:17 +0530

    upate station urls

--- src/data/stations.js
+++ src/data/stations.js
@@ -1,73 +1,23 @@
 export const stationList = [
     {
-        img: '/stations/radiomango.png',
+        img: '/stations/radiomirchi.jpg',
+        name: 'Mirchi Kochi',
+        url: 'http://listen.openstream.co/4626/audio or https://n02.radiojar.com/q6hbcwmx8vzuv.mp3?rj-ttl=5&rj-tok=AAABdW3c0j8AU1QT19VJzvtLWw',
+    },
+    {
+        img: '/stations/clubfm.jpg',
+        name: 'Club FM',
+        url: 'https://sp14.instainternet.com/8050/stream',
+    },
+    {
+        img: '/stations/radiomango.jpg',
         name: 'Radio Mango',
         url: 'https://bcovlive-a.akamaihd.net/19b535b7499a4719a5c19e043063f5d9/ap-southeast-1/6034685947001/playlist.m3u8',
     },
     {
-        img: '/stations/clubfm.png',
-        name: 'Club  FM',
-        url: 'https://playerservices.streamtheworld.com/api/livestream-redirect/CLUBFMUAEAAC.aac',
-    },
-    {
-        img: '/stations/air.jpg',
-        name: 'AIR Kochi FM 102.3',
-        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio045/playlist.m3u8',
-    },
-    {
-        img: '/stations/fmrainbow.jpg',
-        name: 'FM Rainbow Kochi',
-        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio044/playlist.m3u8',
-    },
-    {
-        img: '/stations/air.jpg',
-        name: 'AIR Kozhikode',
-        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio083/playlist.m3u8',
-    },
-    {
-        img: '/stations/airmanjeri.jpg',
-        name: 'Manjeri FM',
-        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio101/playlist.m3u8',
-    },
-    {
-        img: '/stations/air.jpg',
-        name: 'Ananthapuri FM',
-        url: 'https://www.liveradio.es/http://59.90.28.91:88/broadwavehigh.mp3?src=1',
-    },
-    {
-        img: '/stations/air.jpg',
-        name: 'AIR Malayalam Thiruvananthapuram',
-        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio230/playlist.m3u8',
-    },
-    {
-        img: '/stations/air.jpg',
-        name: 'AIR Kannur 101.5',
-        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio173/playlist.m3u8',
-    },
-    {
-        img: '/stations/air.jpg',
-        name: 'AIR Devikulam',
-        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio214/playlist.m3u8',
-    },
-    {
-        img: '/stations/air.jpg',
-        name: 'AIR Kavaratti',
-        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio189/playlist.m3u8',
-    },
-    {
-        img: '/stations/air.jpg',
-        name: 'AIR Thrissur',
-        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio036/chunklist.m3u8',
-    },
-    {
-        img: '/stations/radiokerala.png',
-        name: 'Radio Kerala',
-        url: 'https://www.liveradio.es/http://103.133.180.223:8000/airtime_128',
-    },
-    {
-        img: '/stations/radiomirchi.jpg',
-        name: 'Radio Mirchi',
-        url: 'https://n02.radiojar.com/q6hbcwmx8vzuv.mp3?rj-ttl=5&rj-tok=AAABdW3c0j8AU1QT19VJzvtLWw',
+        img: '/stations/radioasiauae.jpg',
+        name: 'Radio Asia',
+        url: 'https://eu8.fastcast4u.com/proxy/qqyspjqd?mp=/;',
     },
     {
         img: '/stations/radiosuno.jpg',
@@ -75,29 +25,19 @@ export const stationList = [
         url: 'https://playerservices.streamtheworld.com/api/livestream-redirect/SUNO917_SC',
     },
     {
-        img: '/stations/malluradio.jpg',
-        name: 'Mallu Radio',
-        url: 'https://s3.streamworld.co.uk/radio/8020/radio.mp3',
-    },
-    {
-        img: '/stations/radioasiauae.jpg',
-        name: 'Radio Asia UAE',
-        url: 'https://eu8.fastcast4u.com/proxy/qqyspjqd?mp=/;',
+        img: '/stations/air.jpg',
+        name: 'Ananthapuri FM',
+        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio229/playlist.m3u8 or https://securestreams7.autopo.st/?uri=http://59.90.28.91:88/broadwavehigh.mp3 or https://securestreams7.autopo.st/?uri=http://59.90.28.91:88/broadwavehigh.mp3?src=1',
     },
     {
         img: '/stations/pravasibharathi.jpg',
         name: 'Pravasi Bharathi',
-        url: 'https://www.liveradio.es/http://198.27.67.39:8000/pravasiradio.mp3',
+        url: 'https://securestreams7.autopo.st/?uri=http://198.27.67.39:8000/pravasiradio.mp3 or https://securestreams7.autopo.st/?uri=http://198.27.67.39:8000/pravasiradio.mp3',
     },
     {
-        img: '/stations/janavanifm.jpg',
-        name: 'Janavani FM',
-        url: 'https://cors.bitwize.com.lb/http://centova87.instainternet.com:8272/;',
-    },
-    {
-        img: '/stations/radiome.png',
-        name: 'Radio me Malayalam',
-        url: 'https://node-19.zeno.fm/5sm5gtgttzzuv?rj-ttl=5&rj-tok=AAABf0nKBsYA7D7bhOSGxv0IIA',
+        img: '/stations/asharadio.jpg',
+        name: 'Asha Radio',
+        url: 'https://radio.asharadio.net:8000/asharadio',
     },
     {
         img: '/stations/986malayalamradio.jpg',
@@ -107,12 +47,12 @@ export const stationList = [
     {
         img: '/stations/radiolemon.jpg',
         name: 'Radio Lemon',
-        url: 'https://stream.zeno.fm/2qnbwwr7508uv',
+        url: 'https://schedule.radiolemonlive.com:7443/listen/radio_lemon_live_bangalore/live',
     },
     {
-        img: '/stations/radiomix1.jpg',
-        name: 'Radio Mix',
-        url: 'https://stream.zeno.fm/v6yf8nyrwk8uv',
+        img: '/stations/airnew.jpg',
+        name: 'Kochi FM',
+        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio044/playlist.m3u8',
     },
     {
         img: '/stations/cetalks.jpg',
@@ -120,9 +60,14 @@ export const stationList = [
         url: 'https://node-25.zeno.fm/nvh52yer01zuv?rj-ttl=5&rj-tok=AAABdW3i3noAlZtv4wxVBjY6iw',
     },
     {
+        img: '/stations/keralaradio1.jpg',
+        name: 'Radio Kerala',
+        url: 'https://eu4.fastcast4u.com/proxy/radiokerala?mp=/1',
+    },
+    {
         img: '/stations/radiotalky.jpg',
         name: 'Radio Talky',
-        url: 'https://fps1.listen2myradio.com:2199/listen.php?ip=95.154.228.120&port=8048&type=s1',
+        url: 'https://media.streambrothers.com:8172/stream',
     },
     {
         img: '/stations/radiocitymalayalam.jpg',
@@ -130,34 +75,44 @@ export const stationList = [
         url: 'https://prclive4.listenon.in/Malayalam',
     },
     {
-        img: '/stations/helloradio.jpg',
-        name: 'Hello Radio',
-        url: 'https://stream.zenolive.com/taknefee3qzuv.aac',
+        img: '/stations/airnew.jpg',
+        name: 'Kannur FM',
+        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio173/playlist.m3u8',
     },
     {
-        img: '/stations/radiobenziger.jpg',
-        name: 'Radio Benziger',
-        url: 'http://stream.zeno.fm/tuuegh0mqwzuv',
+        img: '/stations/helloradio.jpg',
+        name: 'Hello Radio',
+        url: 'https://stream.zeno.fm/taknefee3qzuv.aac',
+    },
+    {
+        img: '/stations/aahafm.jpg',
+        name: 'Aaha FM',
+        url: 'http://s2.radio.co/s3801784f1/listen',
+    },
+    {
+        img: '/stations/airnew.jpg',
+        name: 'Manjeri FM',
+        url: 'https://air.pc.cdn.bitgravity.com/air/live/pbaudio101/playlist.m3u8',
     },
     {
         img: '/stations/radioneythal.jpg',
         name: 'Radio Neythal',
-        url: 'https://stream.zeno.fm/n2b8yve36qzuv',
+        url: 'https://securestreams7.autopo.st/?uri=http://91.121.82.33:64420/;',
     },
     {
         img: '/stations/radiomattoli.jpg',
         name: 'Radio Mattoli',
-        url: 'https://fps5.listen2myradio.com:2199/listen.php?ip=78.129.139.48&port=8012&type=s1 or https://listen.openstream.co/4635/audio',
+        url: 'https://cast1.my-control-panel.com/proxy/radiomattoli/stream',
     },
     {
         img: '/stations/globalradio.jpg',
         name: 'Global Radio',
-        url: 'https://www.liveradio.es/http://streamasiacdn.atc-labs.com/globalradio.aac',
+        url: 'https://securestreams7.autopo.st/?uri=http://streamasiacdn.atc-labs.com/globalradio.aac',
     },
     {
         img: '/stations/enteradio.jpg',
         name: 'Ente Radio',
-        url: 'https://listen.openstream.co/4620/audio',
+        url: 'https://cast1.my-control-panel.com/proxy/enteradio/stream',
     },
     {
         img: '/stations/radiomediavillage.jpg',
@@ -165,9 +120,9 @@ export const stationList = [
         url: 'https://stream.zeno.fm/a6auvq936qzuv',
     },
     {
-        img: '/stations/asharadio.jpg',
-        name: 'Asha Radio',
-        url: 'https://edge.mixlr.com/channel/gvufg',
+        img: '/stations/janavanifm.jpg',
+        name: 'Janvani FM',
+        url: 'https://securestreams7.autopo.st/?uri=http://centova87.instainternet.com:8272/;',
     },
     {
         img: '/stations/radiomacfast.jpg',
@@ -175,9 +130,14 @@ export const stationList = [
         url: 'https://s2.stationplaylist.com:7080/macfast.aac',
     },
     {
-        img: '/stations/radiovatakara.jpg',
-        name: 'Radio Vatakara',
-        url: 'https://stream.zeno.fm/9k5ku7bhb8zuv',
+        img: '/stations/radiomangalam.jpg',
+        name: 'Radio Mangalam',
+        url: 'https://securestreams7.autopo.st/?uri=http://radiomangalam.in:8000/stream or https://securestreams7.autopo.st/?uri=http://radiomangalam.in:8000/stream',
+    },
+    {
+        img: '/stations/sargakshetrafm.jpg',
+        name: 'Sargakshetra FM',
+        url: 'https://edge.mixlr.com/channel/ugbfe',
     },
     {
         img: '/stations/radiomalayalamusa.jpg',
@@ -187,12 +147,22 @@ export const stationList = [
     {
         img: '/stations/lmrradio.jpg',
         name: 'LMR Radio',
-        url: 'https://streamingv2.shoutcast.com/RadioLMR',
+        url: 'https://ais-edge51-live365-dal02.cdnstream.com/a50671',
+    },
+    {
+        img: '/stations/kschitra.jpg',
+        name: 'KS Chitra',
+        url: 'http://stream.zeno.fm/dubvcz3rkrhvv',
     },
     {
         img: '/stations/mohanlalhits.jpg',
         name: 'Mohanlal hits',
-        url: 'http://stream.zenolive.com/5vva7u4ts0quv',
+        url: 'http://stream.zeno.fm/5vva7u4ts0quv',
+    },
+    {
+        img: '/stations/kjyesudas.jpg',
+        name: 'KJ Yesudas',
+        url: 'https://stream.zeno.fm/9x1sw687nf9uv',
     },
     {
         img: '/stations/radioonattukara.jpg',
@@ -200,9 +170,14 @@ export const stationList = [
         url: 'https://stream.zeno.fm/fu94u25kqd0uv',
     },
     {
-        img: '/stations/radiobeats.jpg',
-        name: 'Radio Beats',
-        url: 'https://streamer.radio.co/sd0b826808/listen',
+        img: '/stations/recastfm.jpg',
+        name: 'Recast FM',
+        url: 'https://usa8.fastcast4u.com/proxy/auralradio?mp=/1/;stream.mp3',
+    },
+    {
+        img: '/stations/radiojaiho.jpg',
+        name: 'Radio Jaiho',
+        url: 'https://eu8.fastcast4u.com/proxy/radiojaiho?mp=/1',
     },
     {
         img: '/stations/radiodelhimalayalam.jpg',
@@ -220,9 +195,9 @@ export const stationList = [
         url: 'https://listen.radioking.com/radio/305023/stream/354512',
     },
     {
-        img: '/stations/pallaviradio.jpg',
-        name: 'Pallavi Radio',
-        url: 'https://pallaviradio.out.airtime.pro/pallaviradio_a',
+        img: '/stations/mozhifm.jpg',
+        name: 'Mozhi FM',
+        url: 'https://stream.zeno.fm/vgd466bfffhvv',
     },
     {
         img: '/stations/gramaphonefm.jpg',
@@ -237,11 +212,11 @@ export const stationList = [
     {
         img: '/stations/pranayamradio.jpg',
         name: 'Pranayam Radio',
-        url: 'https://stream.zenolive.com/g2zves9gdwzuv',
+        url: 'https://stream.zeno.fm/g2zves9gdwzuv',
     },
     {
         img: '/stations/arradio.jpg',
         name: 'AR Radio',
-        url: 'https://stream.zenolive.com/heq754gwxwzuv',
+        url: 'https://stream.zeno.fm/heq754gwxwzuv',
     },
 ];
--- src/scrapper/x-ray.js
+++ src/scrapper/x-ray.js
@@ -1,4 +1,5 @@
 const x = require('x-ray-scraper');
+const axios = require('axios');
 const fs = require('fs');
 
 const channelSelector =
@@ -9,23 +10,70 @@ const scriptSelectorMp3 =
 const scriptSelectorHls =
     'body > div.content-bg > div > div > div.content > div > div > div.single > div.para > div > span > sup > script:nth-child(2)';
 
-x('https://radiosindia.com/malayalamradio.html', channelSelector, [
-    {
-        img: 'a >img@src',
-        name: 'a > p',
-        url: x('a@href', { scriptSelectorMp3, scriptSelectorHls }),
-    },
-]).then(results => {
-    const x = results.map(r => {
-        url = r.url.scriptSelectorMp3 || r.url.scriptSelectorHls;
+async function scrapeStations() {
+    return new Promise((resolve, reject) => {
+        x('https://radiosindia.com/malayalamradio.html', channelSelector, [
+            {
+                img: 'a >img@src',
+                name: 'a > p',
+                url: x('a@href', { scriptSelectorMp3, scriptSelectorHls }),
+            },
+        ])
+            .then(async results => {
+                const data = results.map(r => {
+                    url = r.url.scriptSelectorMp3 || r.url.scriptSelectorHls;
 
-        return {
-            ...r,
-            url: url?.trim().match(`file:"(.*)",`)[1],
-        };
+                    return {
+                        ...r,
+                        url: url?.trim().match(`file:"(.*)",`)[1],
+                    };
+                });
+
+                resolve(data);
+            })
+            .catch(reject);
     });
-    fs.writeFileSync(
-        'src/data/stations.js',
-        `export const stationList = ${JSON.stringify(x)}`
-    );
-});
+}
+
+async function downloadImage(url) {
+    const fileName = url.split('/').pop();
+    console.log(fileName);
+
+    const response = await axios.get(url, {
+        responseType: 'arraybuffer',
+    });
+
+    fs.writeFileSync(`./public/stations/${fileName}`, response.data);
+
+    return `/stations/${fileName}`;
+}
+
+async function updateStations() {
+    try {
+        const stations = await scrapeStations();
+
+        fs.rmdirSync('./public/stations/', { recursive: true });
+        fs.mkdirSync('./public/stations/');
+
+        const dataPromise = stations
+            .filter(station => station.url)
+            .map(async station => {
+                const filePath = await downloadImage(station.img);
+                return {
+                    ...station,
+                    img: filePath,
+                };
+            });
+
+        const data = await Promise.all(dataPromise);
+
+        fs.writeFileSync(
+            'src/data/stations.js',
+            `export const stationList = ${JSON.stringify(data)}`
+        );
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+updateStations();
